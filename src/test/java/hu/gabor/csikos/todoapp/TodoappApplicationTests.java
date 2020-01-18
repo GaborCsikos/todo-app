f4695c4727d5f09a764e7a6bef01726913540843
gcsikos <csiki123@gmail.com>
2020-01-18 15:09:49 +0100

    added structure

---
+++ src/test/java/hu/gabor/csikos/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.gabor.csikos.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
