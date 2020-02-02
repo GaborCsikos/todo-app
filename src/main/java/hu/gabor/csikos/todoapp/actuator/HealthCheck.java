4a6882fdade357438bf490683a87b3050637ec3f
gcsikos <csiki123@gmail.com>
2020-02-02 18:48:16 +0100

    Added spring actuator and devtools

---
+++ src/main/java/hu/gabor/csikos/todoapp/actuator/HealthCheck.java
@@ -0,0 +1,36 @@
+package hu.gabor.csikos.todoapp.actuator;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HealthCheck implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        int errorCode = check(); // perform some specific health check
+        if (errorCode != 0) {
+            return Health.down()
+                    .withDetail("Error Code", errorCode).build();
+        }
+        return Health.up().build();
+    }
+
+    public int check() {
+        // Always returns up
+        return 0;
+    }
+/*
+For Spring security
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(
+            ServerHttpSecurity http) {
+        return http.authorizeExchange()
+                .pathMatchers("/actuator/**").permitAll()
+                .anyExchange().authenticated()
+                .and().build();
+    }
+    */
+
+}
---
+++ src/main/java/hu/gabor/csikos/todoapp/config/EmbeddedRedisServer.java
@@ -0,0 +1,29 @@
+package hu.gabor.csikos.todoapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Configuration
+@Profile("!test")
+public class EmbeddedRedisServer {
+    private RedisServer redisServer;
+
+    public EmbeddedRedisServer(RedisProperties redisProperties) {
+        this.redisServer = new RedisServer(redisProperties.getRedisPort());
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        redisServer.stop();
+    }
+
+}
